53c7f50152c4788311b8a6b914aacf3429713b68
Mehdi Rizvi <mehdirizvi03@gmail.com>
2021-07-17 17:26:35 -0400

    added Components folder for Login module and ProfilePage

---
+++ src/Components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+
+
+
+export default Login
---
+++ src/Components/ProfilePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function ProfilePage() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+
+
+export default ProfilePage

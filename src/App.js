9b019e583b5ac8c834fb77b2e8102c003aac88a6
Mehdi Rizvi <mehdirizvi03@gmail.com>
2021-07-17 16:18:48 -0400

    Fixed main website code to actually display Movie.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,110 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios';
-import Movie from 'movie';
+
+import React, {useState, useEffect} from "react";
+import Movie from './Movie';
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page1";
 
 
 function App() {
-  const [movies, setMovie] = useState([]);
-  const [search, setSearch] = useState('');
 
+    const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
+    useEffect(() => {
+     fetch(FEATURED_API)
+         .then(res => res.json())
 
-    axios.get("https://api.themoviedb.org/3/movie/{movie_id}?api_key=be4d356fb22170def0d9e1ff1612e625&language=en-US")
-        .then(res => {
-          setMovie(res.data);
-          console.log(res.data);
-        })
-        .catch(error => console.log(error));
-  }, []);
+          .then((data) =>{
+              console.log(data);
+              setMovies(data.results);
+          });
 
-  const handleChange = e => {
-    setSearch(e.target.value);
-  };
+    }, []);
 
-  const filter = movies.filter(movie =>
-      movie.name.toLowerCase().includes(search.toLowerCase())
-  );
+    return(
+        <div>
 
-
-  return(
-    <div className="App">
-      <div className="m-search">
-        <h1 className="text"> Search For Movies </h1>
-          <form>
-              <input type="text" placeholder="Search" className="movieInput" onChange={handleChange}/>
-
-          </form>
-      </div>
-        {filter.map(movie => {
-            return (
+            {movies.length > 0 && movies.map((movie)=>(
                 <Movie
                     key={movie.id}
                     name={movie.original_title}
-                    poster={movie.logo_path}
+                    poster={movie.poster_path}
                 />
-            );
-        })}
-    </div>
-  );
+                ))}
+
+        </div>
+    );
+
+
+
 }
 
 export default App;
+
+
+
+
+/*
+
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import Movie from './Movie';
+
+
+function App() {
+    const [movies, setMovie] = useState([]);
+    const [search, setSearch] = useState('');
+
+
+    useEffect(() => {
+
+        axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page1")
+            .then(res => {
+                setMovie(res.data);
+                console.log(res.data);
+            })
+            .catch(error => alert(error));
+    }, []);
+
+    const handleChange = e => {
+        setSearch(e.target.value);
+    };
+
+    const filter = movies.filter(movie =>
+        movie.name.toLowerCase().includes(search.toLowerCase())
+    );
+
+
+    return(
+
+        <div className="App">
+
+            <div className="m-search">
+                <h1 className="text"> Search For Movies </h1>
+
+
+
+                <form>
+                    <input type="text" placeholder="Search" className="movieInput" onChange={handleChange}/>
+
+                </form>
+
+
+
+            </div>
+            {filter.map(movie => {
+                return (
+                    <Movie
+
+                        key={movie.id}
+                        name={movie.original_title}
+                        poster={movie.poster_path}
+                    />
+                );
+            })}
+        </div>
+    );
+}
+
+export default App;
+
+*/

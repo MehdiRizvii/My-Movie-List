00cf59bfd94548e0593ea510bb2f7b9a8b9f77ee
Mehdi Rizvi <mehdirizvi03@gmail.com>
2021-07-22 14:55:07 -0400

    Added functional login page using Auth0

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
+ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+ import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+
+ const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+ const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+ ReactDOM.render(
+     <Auth0Provider
+         domain={domain}
+         clientId={clientId}
+         redirectUri={window.location.origin}>
+       <App />
+     </Auth0Provider>,
+     document.getElementById('root')
+ );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

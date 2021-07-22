be2fe11836e0ff4a4e564efac1a2c49d649405b9
Mehdi Rizvi <mehdirizvi03@gmail.com>
2021-07-22 14:56:18 -0400

    Added functional login widget/button

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {useAuth0} from '@auth0/auth0-react';
 
-function Login() {
+
+
+const Login = () => {
+
+    const {loginWithRedirect} = useAuth0();
+
     return(
-        <div>
+<button onClick={()=> loginWithRedirect()}> Login</button>
 
-        </div>
     )
 }
+console.log(Login);
+
 
 
 
